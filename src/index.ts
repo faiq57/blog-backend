93bd095097ec3351e2a2ebfe4cd462466c768774
Faiq Izhal <f.izhal@student.unsw.edu.au>
2026-03-01 05:26:54 +1100

    Written a route that allow comments to be written

--- src/index.ts
+++ src/index.ts
@@ -18,4 +18,27 @@ app.get('/comments', async (c) => {
   return c.json(result);
 })
 
+app.post('/comments', async (c) => {
+  const body = await c.req.json()
+  if (!Object.hasOwn(body, 'author')) {
+    return c.text('Your comment needs an author')
+  } else if (!Object.hasOwn(body, 'content')) {
+    return c.text('You comment need stuff')
+  }
+
+  const author = body.author.toString()
+  const content = body.content.toString()
+
+  if (author.length == 0 || author.length > 256) {
+    return c.text('You either need a name or your name is too long')
+  } else if (content.length == 0 || content.length > 1024) {
+    return c.text('You either need to comment anything or your comment is too long')
+  }
+
+  const db = drizzle(c.env.DB);
+  const result = await db.insert(comments).values({author: author, content: content})
+
+  return c.text('Your comment is successfully published')
+})
+
 export default app
