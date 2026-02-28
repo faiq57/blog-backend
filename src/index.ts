64466788c9fd9e6cc4fa5154664cdbd16a971ed2
Faiq Izhal <f.izhal@student.unsw.edu.au>
2026-02-28 18:26:22 +1100

    Configured types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import { Hono } from 'hono'
 
+export type Env = {
+  DB: D1Database;
+}
+
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.get('/comments', (c) => {
+  return c.text("Hello again!")
 })
 
 export default app
